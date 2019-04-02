5588a6a3a90fc19f4b8d0695aae60f25432f6630
Vincent Kuhlen <vkuhlen@uni-bremen.de>
2019-04-02 16:27:49 +0200

    add gzip compression

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  presets: [
-    '@vue/app'
-  ]
+    presets: [
+        ['@vue/app', {
+            useBuiltIns: false,
+            jsx: false,
+            corejs: "3"
+        }]
+    ]
 }
