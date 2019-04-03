ff25d387b71cde820cee877c470948cfb4a25b51
Vincent Kuhlen <vkuhlen@uni-bremen.de>
2019-04-03 12:52:31 +0200

    disable gzip and source maps

--- vue.config.js
+++ vue.config.js
@@ -2,15 +2,16 @@ const CompressionWebpackPlugin = require('compression-webpack-plugin')
 const productionGzipExtensions = ['js', 'css']
 
 module.exports = {
-    configureWebpack: {
-        plugins: [
-            new CompressionWebpackPlugin({
-                filename: '[path].gz[query]',
-                algorithm: 'gzip',
-                test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
-                threshold: 10240,
-                minRatio: 0.8
-            })
-        ]
-    }
+    productionSourceMap: false,
+    // configureWebpack: {
+    //     plugins: [
+    //         new CompressionWebpackPlugin({
+    //             filename: '[path].gz[query]',
+    //             algorithm: 'gzip',
+    //             test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+    //             threshold: 10240,
+    //             minRatio: 0.8
+    //         })
+    //     ]
+    // }
 }
